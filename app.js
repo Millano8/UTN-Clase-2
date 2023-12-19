3130de7b497cce3cc78e8a5277feea6666b7353f
unknown <fran.millan95@gmail.com>
2023-12-19 17:08:25 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const readlineSync = require("readline-sync");
+const {generarNumeroAleatorio, verificarAdivinanza} = require("./adivinanza");
+
+
+const obtenerNumeroUsuario = () => {
+    return readlineSync.question('Ingresa un numero: ')
+};
+
+const juegoAdivinanza = () => {
+    const numeroSecreto = generarNumeroAleatorio();
+    let numeroAdivinado = 0;
+
+
+    console.log('¡Bienvenido a Adivina el numero secreto!')
+    console.log('Intenta adivinar el numero que va de 1 a 100.\n')
+
+    while (numeroAdivinado != numeroSecreto) {
+        numeroAdivinado = obtenerNumeroUsuario();
+        verificarAdivinanza(numeroSecreto, numeroAdivinado)
+    }
+}
+
+juegoAdivinanza()
